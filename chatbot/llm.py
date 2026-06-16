7d5f56a7c96e4b84a33f2e67b5c72536bc2104cc
Neha Sirohi <T4291@novel.com>
2026-06-16 11:24:17 +0530

    add streaming effect

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -1,15 +1,26 @@
-from ollama import chat
-from ollama import ChatResponse 
+# from ollama import chat
+# from ollama import ChatResponse 
 
+# def generate_response(message):
+#     response: ChatResponse = chat(model='smollm2:360m',messages=[
+#         {
+#             'role':'user',
+#             'content':message
+#          },
+#     ])
+#     return response.message.content
+
+
+from ollama import chat 
 def generate_response(message):
-    response: ChatResponse = chat(model='smollm2:360m',messages=[
+    stream = chat(model='smollm2:360m',messages=[
         {
             'role':'user',
             'content':message
-         },
-    ])
-    return response.message.content
-    
-
-
+        }
+    ],
+    stream=True
+    )
+    for chunk in stream:
+        print(chunk.message.content,end='',flush=True)
 
