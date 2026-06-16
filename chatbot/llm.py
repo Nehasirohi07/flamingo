28fea9e73b2c64dbb85ad684c60fc4525f9a4eea
Neha Sirohi <T4291@novel.com>
2026-06-16 13:30:56 +0530

    add conversation to memory

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -11,19 +11,30 @@
 #     return response.message.content
 
 
-from ollama import chat 
+from ollama import chat
+from chatbot.memory import conversation 
 def generate_response(message):
-    stream = chat(model='smollm2:360m',messages=[
-        {
-            'role':'user',
-            'content':message
+    conversation.append(
+        { 'role': 'user',
+          'content': message
         }
-    ],
+    )
+    stream = chat(model='smollm2:360m',messages=conversation,
+    #     {
+    #         'role':'user',
+    #         'content': message
+    #     }
+    # ],
     stream=True
     )
     answer = ""
     for chunk in stream:
         print(chunk.message.content,end='',flush=True)
         answer += chunk.message.content
+    conversation.append(
+        { 'role': 'assistant',
+          'content': answer
+        }
+    )
     return answer
 
